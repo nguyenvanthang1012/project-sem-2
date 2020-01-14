2c91bfba65b1f83e61bb2d1e3584627f91339b8e
nguyenvanthang1012 <59723664+nguyenvanthang1012@users.noreply.github.com>
2020-01-14 11:44:37 +0700

    update database.sql file

--- database.sql
+++ database.sql
@@ -5,29 +5,36 @@ go
 create table category(
 	cat_id int identity primary key,
 	name nvarchar(100) not null,
-	status bit default(1)
+	status bit default(0)
+)
+go
+create table admin(
+	id int identity primary key,
+	username varchar(20) not null unique,
+	sdt varchar(10) not null unique,
+	password varchar(255) not null
 )
 go
 create table book(
 	book_id int identity primary key,
 	name nvarchar(100) not null,
 	cat_id int foreign key references category(cat_id),
-	author nvarchar(100) default(N'no name'),
-	description text not null,
+	author nvarchar(100) default('no name'),
+	description ntext not null,
 	publication_date date check(publication_date < GETDATE()),
+	date_create datetime,
 	quantity int default(0) not null check(quantity >= 0),
-	price int default(0) not null check(price >= 0),
-	status tinyint default(0),
-	note text,
+	price float default(0) not null check(price >= 0),
+	status tinyint default(0)
 )
 go
 create table student(
-	student_id int identity primary key,
+	id int identity primary key,
+	student_id varchar(10) not null unique,
 	name nvarchar(100) not null,
+	class_name varchar(100) not null,
 	phone varchar(10) not null unique,
-	email varchar(100) not null unique,
 	dateofbirth date not null,
-	home_town nvarchar(255) not null,
 	status tinyint default(0),
 )
 go
@@ -37,27 +44,33 @@ create table employee(
 	name nvarchar(100) not null,
 	phone varchar(10) not null unique,
 	email varchar(50) not null unique,
-	home_town nvarchar(255) not null
+	home_town nvarchar(255) not null,
+	status bit default(0)
 )
 go
 create table borrow(
 	borrow_id int identity primary key,
-	student_id int foreign key references student(student_id),
+	student_id int foreign key references student(id),
 	employee_id int foreign key references employee(employee_id),
-	date_borrow date  not null,
+	date_borrow datetime  not null,
+	status bit default(0)
 )
 go
 create table borrow_detail(
 	borrow_detail_id int identity primary key,
 	borrow_id int foreign key references borrow(borrow_id),
 	book_id int foreign key references book(book_id),
-	date_return date,
+	date_appointment date not null,
+	date_return datetime,
+	status bit default(0)
 )
 go
 create table punish(
 	punish_id int identity primary key,
-	student_id int foreign key references student(student_id),
-	money int check(money > 0),
-	reason text not null,
-	employee_id int foreign key references employee(employee_id)
+	student_id int foreign key references student(id),
+	money float check(money > 0),
+	reason ntext not null,
+	employee_id int foreign key references employee(employee_id),
+	borrow_detail_id int foreign key references borrow_detail(borrow_detail_id),
+	status bit default(0),
 )
